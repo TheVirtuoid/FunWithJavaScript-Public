4a088c3fcc49c675fc22f5ffce04b4a1a88ec5da
TheVirtuoid <thevirtuoid@gmail.com>
2025-04-11 18:56:50 -0400

    Scaffold racing-game files

---
+++ games/racing-game/cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4173',
+    specPattern: [
+      "tests/**/*.cy.js",
+    ],
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ games/racing-game/src/racing-game.js
@@ -0,0 +1,3 @@
+import './css/racing-game.pcss';
+
+document.getElementById('javascript-alive').textContent = 'And JavaScript is also alive!';
