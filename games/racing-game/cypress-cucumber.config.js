904102af069500f3bb74f0b553f19eb05ff82712
TheVirtuoid <thevirtuoid@gmail.com>
2025-04-20 20:14:59 -0400

    racing: fixed a few issues with the feature testing

--- games/racing-game/cypress-cucumber.config.js
+++ games/racing-game/cypress-cucumber.config.js
@@ -22,5 +22,6 @@ export default defineConfig({
 
       // Make sure to return the config object as it might have been modified by the plugin.
       return config;
-    },  },
+    },
+  },
 });
--- games/racing-game/src/racing-game.js
+++ games/racing-game/src/racing-game.js
@@ -1,3 +1,7 @@
 import './css/racing-game.pcss';
 
 document.getElementById('javascript-alive').textContent = 'And JavaScript is also alive!';
+
+document.getElementById('hello-world').addEventListener('click', (e) => {
+	document.getElementById('hello-world-text').classList.remove('hidden');
+});
