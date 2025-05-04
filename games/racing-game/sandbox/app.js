8d52ef84d635652ea3b1ca95d7aff4a126a188e6
TheVirtuoid <thevirtuoid@gmail.com>
2025-05-03 20:50:11 -0400

    racing: Refactored the Sandbox a little to experiment with

--- games/racing-game/sandbox/app.js
+++ games/racing-game/sandbox/app.js
@@ -2,16 +2,19 @@ import {
 	ArcRotateCamera, Color3,
 	Engine,
 	HavokPlugin,
-	HemisphericLight, Matrix, Mesh,
+	HemisphericLight,
 	MeshBuilder,
 	PhysicsAggregate, PhysicsShapeType,
-	Scene, StandardMaterial, Tools,
+	Scene, StandardMaterial,
 	Vector3
 } from "@babylonjs/core";
 import { Inspector } from '@babylonjs/inspector';
 import HavokPhysics from "@babylonjs/havok";
+import {renderCurve, renderStraight} from "./utilities.js";
 
 export default class App {
+
+	static GRAVITY = 1;
 	#engine;
 	#scene;
 	#canvas;
@@ -24,14 +27,18 @@ export default class App {
 	#sphereAggregate;
 	#groundAggregate;
 
+	#sphere1;
+	#sphere2;
+
 	#physicsPlugin;
 
 	#gravityVector = new Vector3(0, -9.81, 0);
 
-	#renderLoopHandle;
+	#sx;
+	#sy;
+	#sz;
 
-	bp1;
-	bp2;
+	#renderLoopHandle;
 
 	constructor() {
 		this.#canvas = document.getElementById('world');
@@ -65,280 +72,124 @@ export default class App {
 		})
 	}
 
-	bezierCurve3d(args = {}) {
-		const { startPoint, controlPoint1, controlPoint2, endPoint, segments = 10, zAdjust = 0 } = args;
-		const points = [];
-		for (let t = 0; t <= 1; t += 1 / segments) {
-			const x = Math.pow(1 - t, 3) * startPoint.x +
-				3 * Math.pow(1 - t, 2) * t * controlPoint1.x +
-				3 * (1 - t) * Math.pow(t, 2) * controlPoint2.x +
-				Math.pow(t, 3) * endPoint.x;
-
-			const y = Math.pow(1 - t, 3) * startPoint.y +
-				3 * Math.pow(1 - t, 2) * t * controlPoint1.y +
-				3 * (1 - t) * Math.pow(t, 2) * controlPoint2.y +
-				Math.pow(t, 3) * endPoint.y;
-
-			const z = Math.pow(1 - t, 3) * startPoint.z +
-				3 * Math.pow(1 - t, 2) * t * controlPoint1.z +
-				3 * (1 - t) * Math.pow(t, 2) * controlPoint2.z +
-				Math.pow(t, 3) * endPoint.z;
-
-			points.push({ x, y, z });
-		}
-		points.push({ x: endPoint.x, y: endPoint.y, z: endPoint.z });
-		return points;
-	}
-
 	async #addToScene() {
+		this.#sx = 0;
+		this.#sy = 39;
+		this.#sz = -55;
 		this.#camera = new ArcRotateCamera("Camera", Math.PI, Math.PI / 2.5, 20, Vector3.Zero(), this.#scene);
 		this.#camera.attachControl(this.#canvas, true);
 		this.#light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), this.#scene);
 
 		this.#sphere = MeshBuilder.CreateSphere("sphere", { diameter: .5 }, this.#scene);
-		this.#sphere.position.y = 39.25;
-		this.#sphere.position.z = -54.5;
+		const sphereMaterial = new StandardMaterial("red", this.#scene);
+		sphereMaterial.diffuseColor = new Color3(1, 0, 0);
+		this.#sphere.material = sphereMaterial;
+		this.#sphere.position.y = this.#sy + .3;
+		this.#sphere.position.z = this.#sz + .5;
+		this.#sphere.position.x = this.#sx;
+
+		this.#sphere1 = MeshBuilder.CreateSphere("sphere1", { diameter: .5 }, this.#scene);
+		const sphere1Material = new StandardMaterial("blue", this.#scene);
+		sphere1Material.diffuseColor = new Color3(0, 0, 1);
+		this.#sphere1.material = sphere1Material;
+		this.#sphere1.position.y = this.#sy + .3;
+		// this.#sphere1.position.z = this.#sz - 14.5;
+		this.#sphere1.position.z = this.#sz + .5;
+		this.#sphere1.position.x = this.#sx - 1;
+
+		this.#sphere2 = MeshBuilder.CreateSphere("sphere2", { diameter: .5 }, this.#scene);
+		const sphere2Material = new StandardMaterial("green", this.#scene);
+		sphere2Material.diffuseColor = new Color3(0, 1, 0);
+		this.#sphere2.material = sphere2Material;
+		this.#sphere2.position.y = this.#sy + .3;
+		this.#sphere2.position.z = this.#sz + .5;
+		this.#sphere2.position.x = this.#sx + 1;
 
 		this.#ground = MeshBuilder.CreateBox("ground", { width: 60, height: .1, depth: 60}, this.#scene);
-		const groundMaterial = new StandardMaterial("green", this.#scene);
+		const groundMaterial = new StandardMaterial("grass", this.#scene);
 		groundMaterial.diffuseColor = new Color3(0, .25, 0);
 		this.#ground.material = groundMaterial;
-		this.#ground.position.y = -0.06;
-		this.#ground.position.x = -20;
+		this.#ground.position.y = this.#sy - 39.06;
+		this.#ground.position.x = this.#sx - 20;
 
 		this.#physicsPlugin = new HavokPlugin(true, await HavokPhysics());
 		this.#scene.enablePhysics(this.#gravityVector, this.#physicsPlugin);
 
 		// Create a sphere shape and the associated body. Size will be determined automatically.
-		this.#sphereAggregate = new PhysicsAggregate(this.#sphere, PhysicsShapeType.SPHERE, { mass: 1, restitution: 0, friction: .05 }, this.#scene);
+		this.#sphereAggregate = new PhysicsAggregate(this.#sphere, PhysicsShapeType.SPHERE, { mass: App.GRAVITY, restitution: 0, friction: .05 }, this.#scene);
+		new PhysicsAggregate(this.#sphere1, PhysicsShapeType.SPHERE, { mass: App.GRAVITY, restitution: 0, friction: .05 }, this.#scene);
+		new PhysicsAggregate(this.#sphere2, PhysicsShapeType.SPHERE, { mass: App.GRAVITY, restitution: 0, friction: .05 }, this.#scene);
 		// Create a static box shape.
-		this.#groundAggregate = new PhysicsAggregate(this.#ground, PhysicsShapeType.BOX, { mass: 0 }, this.#scene);
+		this.#groundAggregate = new PhysicsAggregate(this.#ground, PhysicsShapeType.BOX, { mass: 0, friction: 1 }, this.#scene);
 
 		let straightRoad = this.generateAStraightRoad();
-		straightRoad.position.y = 9;
-		straightRoad.position.z = -15;
 		const straightRoadAggregate = new PhysicsAggregate(
 			straightRoad,
 			PhysicsShapeType.MESH,
 			{ mass: 0 }, this.#scene
 		);
+
 		let curveRoad = this.generateACurve();
-		// curveRoad.position.z = -1;
 		const curveRoadAggregate = new PhysicsAggregate(
 			curveRoad,
 			PhysicsShapeType.MESH,
 			{ mass: 0 }, this.#scene
 		);
-
-		let railingArray = this.generateRailing(this.railingArray, 1.5);
-		let railing = MeshBuilder.CreateRibbon("railing", {
-			pathArray: [this.railingArray, railingArray],
-			sideOrientation: Mesh.DOUBLESIDE,
-			updatable: true
-		}, this.#scene);
-		const railingAggregate = new PhysicsAggregate(
-			railing,
-			PhysicsShapeType.MESH,
-			{ mass: 0 }, this.#scene
-		);
-
-		let innerRailingArray = this.generateRailing(this.innerRail, .6);
-		let innerRailing = MeshBuilder.CreateRibbon("railing", {
-			pathArray: [this.innerRail, innerRailingArray],
-			sideOrientation: Mesh.DOUBLESIDE,
-			updatable: true
-		}, this.#scene);
-		const innerRailingAggregate = new PhysicsAggregate(
-			innerRailing,
-			PhysicsShapeType.MESH,
-			{ mass: 0 }, this.#scene
-		);
-
-
 	}
 
 	generateAStraightRoad() {
-		const startPoint = { x: 0, y: 30, z: -40 };
-		const controlPoint1 = { x: 0, y: -.75, z: 4 };
-		const controlPoint2 = { x: 0, y: -9, z: 6 };
-		const endPoint = { x: 0, y: -9, z: 15};
+		// const startPoint = { x: this.#sx, y: this.#sy, z: -40 };
+		 const startPoint = { x: this.#sx, y: this.#sy, z: this.#sz };
+		const controlPoint1 = { x: this.#sx, y: this.#sy - 29.25, z: this.#sz + 40 };
+		const controlPoint2 = { x: this.#sx, y: this.#sy - 39, z: this.#sz + 42 };
+		const endPoint = { x: this.#sx, y: this.#sy - 39, z: this.#sz + 55};
 		const segments = 100;
-
-		const originalPoints = this.bezierCurve3d({ startPoint, controlPoint1, controlPoint2, endPoint, segments})
-
-		const offsetDistance = 2;
-		const { offsetPoints1, offsetPoints2 } = this.generateOffsetPoints(originalPoints, offsetDistance);
-
-		return MeshBuilder.CreateRibbon("ribbon", {
-			pathArray: [offsetPoints1, offsetPoints2],
-			sideOrientation: Mesh.DOUBLESIDE,
-			updatable: true
-		}, this.#scene);
-
+		const trackWidth = 4;
+		return renderStraight({ startPoint, controlPoint1, controlPoint2, endPoint, segments, trackWidth, scene: this.#scene });
 	}
 
 	generateACurve() {
+		/*
+		 180 degrees
+		*/
+		const sy = this.#sy - 39;
+		const sz = this.#sz + 55;
+		const startPoint = { x: this.#sx, y: sy, z: sz };
+		const controlPoint1 = { x: this.#sx + .7, y: sy, z: sz + 25 };
+		const controlPoint2 = { x: this.#sx - 30.7, y: sy, z: sz + 25 };
+		const endPoint = { x: this.#sx - 30, y: sy, z: sz };
+		const angle = 75;
+		const firstGuardRailScale = { startScale: .6, endScale: .6 };
+		const secondGuardRailScale = { startScale: .6, endScale: 2 };
+		const segments = 100;
+		const trackWidth = 4;
 
 		/*
-		Here's a more detailed explanation:
-			Start and End Points: Let's say the start point is (0, 0) and the end point is (x, 0).
-			Midpoint: The midpoint of the start and end points would be (x/2, 0).
-			Control Points: To create a 180-degree curve, the control points need to be placed symmetrically around this midpoint.
-				For a cubic Bezier curve, you'd need two control points, one for the start and one for the end of the curve.
-					Control Point 1: This point would be a certain distance away from the start point (0, 0) and the midpoint. Let's say it's at (x1, y1).
-					Control Point 2: This point would be the same distance away from the end point (x, 0) and the midpoint, but on the opposite side. Let's say it's at (x2, -y1).
-			Symmetry: The x-coordinates of the control points should be such that (x1 + x2) / 2 = x/2 (the midpoint's x-coordinate).
-				The y-coordinate of the second control point will be the negative of the y-coordinate of the first control point to
-				ensure the curve bends upwards and then downwards.
+		90 degrees
 
-		 */
 		const startPoint = { x: 0, y: 0, z: 0 };
-		/*const controlPoint1 = { x: .7, y: 0, z: 25 };
-		const controlPoint2 = { x: -30.7, y: 0, z: 25 };*/
-		const controlPoint1 = { x: .7, y: 0, z: 25 };
-		const controlPoint2 = { x: -30.7, y: 0, z: 25 };
-		const endPoint = { x: -30, y: 0, z: 0 };
+		const controlPoint1 = { x: 0, y: 0, z: 10};
+		const controlPoint2 = { x: -10, y: 0, z: 15 };
+		const endPoint = { x: -15, y: 0, z: 15 };
+		const angle = 45;
 		const segments = 100;
+		const trackWidth = 4;
+		const firstGuardRailScale = { startScale: .6, endScale: .6 };
+		const secondGuardRailScale = { startScale: .6, endScale: 2 };
 
-		const originalPoints = this.bezierCurve3d({ startPoint, controlPoint1, controlPoint2, endPoint, segments });
-		const offsetDistance = 2;
-		const maxBankAngle = 75; // Maximum bank angle in degrees
-
-		const { offsetPoints1, offsetPoints2 } = this.generateOffsetPointsWithBanking(originalPoints, offsetDistance, maxBankAngle);
-		this.railingArray = offsetPoints2.map((point => new Vector3(point.x, point.y, point.z)));
-		this.innerRail = offsetPoints1.map((point) => new Vector3(point.x, point.y, point.z));
-		return MeshBuilder.CreateRibbon("ribbon", {
-			pathArray: [offsetPoints1, offsetPoints2],
-			sideOrientation: Mesh.DOUBLESIDE,
-			updatable: true
-		}, this.#scene);
-	}
-
-
-	generateOffsetPoints(points, offsetDistance) {
-		const offsetPoints1 = [];
-		const offsetPoints2 = [];
-
-		for (let i = 0; i < points.length - 1; i++) {
-			const current = points[i];
-			const next = points[i + 1];
-
-			// Calculate the direction vector between the current and next points
-			const direction = new Vector3(next.x - current.x, next.y - current.y, next.z - current.z).normalize();
-
-			// Calculate a perpendicular vector (cross product with an arbitrary vector)
-			const perpendicular = Vector3.Cross(direction, Vector3.Up()).normalize();
-
-			// Offset the current point in both directions
-			const offset1 = new Vector3(
-				current.x + perpendicular.x * offsetDistance,
-				current.y + perpendicular.y * offsetDistance,
-				current.z + perpendicular.z * offsetDistance
-			);
-
-			const offset2 = new Vector3(
-				current.x - perpendicular.x * offsetDistance,
-				current.y - perpendicular.y * offsetDistance,
-				current.z - perpendicular.z * offsetDistance
-			);
-
-			offsetPoints1.push(offset1);
-			offsetPoints2.push(offset2);
-		}
-
-		// Add the last point offsets
-		const last = points[points.length - 1];
-		const secondLast = points[points.length - 2];
-		const lastDirection = new Vector3(last.x - secondLast.x, last.y - secondLast.y, last.z - secondLast.z).normalize();
-		const lastPerpendicular = Vector3.Cross(lastDirection, Vector3.Up()).normalize();
-
-		offsetPoints1.push(new Vector3(
-			last.x + lastPerpendicular.x * offsetDistance,
-			last.y + lastPerpendicular.y * offsetDistance,
-			last.z + lastPerpendicular.z * offsetDistance
-		));
-
-		offsetPoints2.push(new Vector3(
-			last.x - lastPerpendicular.x * offsetDistance,
-			last.y - lastPerpendicular.y * offsetDistance,
-			last.z - lastPerpendicular.z * offsetDistance
-		));
-
-		return { offsetPoints1, offsetPoints2 };
-	}
-
-	generateOffsetPointsWithBanking(points, offsetDistance, maxBankAngle) {
-		const offsetPoints1 = [];
-		const offsetPoints2 = [];
-		const totalPoints = points.length;
-
-		for (let i = 0; i < totalPoints - 1; i++) {
-			const current = points[i];
-			const next = points[i + 1];
-
-			// Calculate the direction vector between the current and next points
-			const direction = new Vector3(next.x - current.x, next.y - current.y, next.z - current.z).normalize();
-
-			// Calculate a perpendicular vector (cross product with an arbitrary vector)
-			let perpendicular = Vector3.Cross(direction, Vector3.Up()).normalize();
-
-			// Apply banking: Rotate the perpendicular vector based on the current point's position
-			const bankAngle = maxBankAngle * Math.sin((Math.PI * i) / (totalPoints - 1)); // Smooth banking curve
-			const rotationMatrix = Matrix.RotationAxis(direction, Tools.ToRadians(bankAngle));
-			perpendicular = Vector3.TransformCoordinates(perpendicular, rotationMatrix);
-
-			// Offset the current point in both directions
-			const offset1 = new Vector3(
-				current.x + perpendicular.x * offsetDistance,
-				// current.y + perpendicular.y * offsetDistance,
-				current.y,
-				current.z + perpendicular.z * offsetDistance
-			);
-
-			const offset2 = new Vector3(
-				current.x - perpendicular.x * offsetDistance,
-				// current.y - perpendicular.y * offsetDistance,
-				current.y - perpendicular.y * offsetDistance * 2,
-				current.z - perpendicular.z * offsetDistance
-			);
-
-			offsetPoints1.push(offset1);
-			offsetPoints2.push(offset2);
-		}
-
-		// Add the last point offsets
-		const last = points[totalPoints - 1];
-		const secondLast = points[totalPoints - 2];
-		const lastDirection = new Vector3(last.x - secondLast.x, last.y - secondLast.y, last.z - secondLast.z).normalize();
-		let lastPerpendicular = Vector3.Cross(lastDirection, Vector3.Up()).normalize();
-
-		const lastRotationMatrix = Matrix.RotationAxis(lastDirection, Tools.ToRadians(0)); // No bank at the end
-		lastPerpendicular = Vector3.TransformCoordinates(lastPerpendicular, lastRotationMatrix);
-
-		offsetPoints1.push(new Vector3(
-			last.x + lastPerpendicular.x * offsetDistance,
-			// last.y + lastPerpendicular.y * offsetDistance,
-			last.y,
-			last.z + lastPerpendicular.z * offsetDistance
-		));
-
-		offsetPoints2.push(new Vector3(
-			last.x - lastPerpendicular.x * offsetDistance,
-			// last.y - lastPerpendicular.y * offsetDistance,
-			last.y - lastPerpendicular.y * offsetDistance * 2,
-			last.z - lastPerpendicular.z * offsetDistance
-		));
-
-		return { offsetPoints1, offsetPoints2 };
-	}
-
-	generateRailing(edge, scale = .4) {
-		const railing = [];
-		for(let i = 0; i < edge.length; i++) {
-			const point = edge[i];
-			railing.push(new Vector3(point.x, point.y + scale, point.z ));
-		}
-		return railing;
+		*/
+		return renderCurve({
+			startPoint,
+			controlPoint1,
+			controlPoint2,
+			endPoint,
+			segments,
+			trackWidth,
+			angle,
+			firstGuardRailScale,
+			secondGuardRailScale,
+			scene: this.#scene
+		});
 	}
 
 }
---
+++ games/racing-game/sandbox/utilities.js
@@ -0,0 +1,230 @@
+import {
+	Matrix,
+	Mesh,
+	MeshBuilder,
+	Tools,
+	Vector3
+} from "@babylonjs/core";
+
+export function bezierCurve3d(args = {}) {
+	const { startPoint, controlPoint1, controlPoint2, endPoint, segments = 10, zAdjust = 0 } = args;
+	const points = [];
+	for (let t = 0; t <= 1; t += 1 / segments) {
+		const x = Math.pow(1 - t, 3) * startPoint.x +
+			3 * Math.pow(1 - t, 2) * t * controlPoint1.x +
+			3 * (1 - t) * Math.pow(t, 2) * controlPoint2.x +
+			Math.pow(t, 3) * endPoint.x;
+
+		const y = Math.pow(1 - t, 3) * startPoint.y +
+			3 * Math.pow(1 - t, 2) * t * controlPoint1.y +
+			3 * (1 - t) * Math.pow(t, 2) * controlPoint2.y +
+			Math.pow(t, 3) * endPoint.y;
+
+		const z = Math.pow(1 - t, 3) * startPoint.z +
+			3 * Math.pow(1 - t, 2) * t * controlPoint1.z +
+			3 * (1 - t) * Math.pow(t, 2) * controlPoint2.z +
+			Math.pow(t, 3) * endPoint.z;
+
+		points.push({ x, y, z });
+	}
+	points.push({ x: endPoint.x, y: endPoint.y, z: endPoint.z });
+	return points;
+}
+
+export function renderStraight(args = {}) {
+	const {
+		startPoint,
+		controlPoint1,
+		controlPoint2,
+		endPoint,
+		scene,
+		roadWidth = 4,
+		segments = 100,
+		firstGuardRailScale = { startScale: .6, endScale: .6 },
+		secondGuardRailScale = { startScale: .6, endScale: .6 }
+	} = args;
+	const originalPoints = bezierCurve3d({ startPoint, controlPoint1, controlPoint2, endPoint, segments});
+	const offsetDistance = roadWidth / 2;
+	const { offsetPoints1, offsetPoints2 } = generateOffsetPoints(originalPoints, offsetDistance);
+	const firstGuardRailPoints = offsetPoints1.map((point) => new Vector3(point.x, point.y, point.z));
+	const secondGuardRailPoints = offsetPoints2.map((point => new Vector3(point.x, point.y, point.z)));
+	const firstGuardRail = generateRailing(firstGuardRailPoints, firstGuardRailScale.startScale, firstGuardRailScale.endScale);
+	const secondGuardRail = generateRailing(secondGuardRailPoints, secondGuardRailScale.startScale, secondGuardRailScale.endScale);
+
+	const lastPoint = originalPoints.length - 1;
+	console.log('---------------------------------------------straight-');
+	console.log(lastPoint, firstGuardRail[lastPoint], offsetPoints1[lastPoint], offsetPoints2[lastPoint], secondGuardRail[lastPoint]);
+	return MeshBuilder.CreateRibbon("ribbon", {
+		pathArray: [firstGuardRail, offsetPoints1, offsetPoints2, secondGuardRail],
+		sideOrientation: Mesh.DOUBLESIDE,
+		updatable: true
+	}, scene);
+}
+
+export function renderCurve(args) {
+	const {
+		startPoint,
+		controlPoint1,
+		controlPoint2,
+		endPoint,
+		scene,
+		angle,
+		roadWidth = 4,
+		segments = 100,
+		firstGuardRailScale = { startScale: .6, endScale: .6 },
+		secondGuardRailScale = { startScale: .6, endScale: 2 }
+	} = args;
+
+	const originalPoints = bezierCurve3d({ startPoint, controlPoint1, controlPoint2, endPoint, segments });
+	const offsetDistance = roadWidth / 2;
+
+	const { offsetPoints1, offsetPoints2 } = generateOffsetPointsWithBanking(originalPoints, offsetDistance, angle);
+	const firstGuardRailPoints = offsetPoints1.map((point) => new Vector3(point.x, point.y, point.z));
+	const secondGuardRailPoints = offsetPoints2.map((point => new Vector3(point.x, point.y, point.z)));
+	const firstGuardRail = generateRailing(firstGuardRailPoints, firstGuardRailScale.startScale, firstGuardRailScale.endScale);
+	const secondGuardRail = generateRailing(secondGuardRailPoints, secondGuardRailScale.startScale, secondGuardRailScale.endScale);
+	console.log('---------------------------------------------curve-');
+	console.log(firstGuardRail[0], offsetPoints1[0], offsetPoints2[0], secondGuardRail[0]);
+
+	return MeshBuilder.CreateRibbon("ribbon", {
+		pathArray: [firstGuardRail, offsetPoints1, offsetPoints2, secondGuardRail],
+		sideOrientation: Mesh.DOUBLESIDE,
+		updatable: true
+	}, scene);
+}
+
+export function generateRailing(edge, startScale = .4, endScale = .4) {
+	const railing = [];
+	const front = edge.length / 4;
+	const back = front * 3;
+	const scaleDistance = (endScale - startScale) / front;
+	for(let i = 0; i < edge.length; i++) {
+		const point = edge[i];
+		let scaleY = endScale;
+		if (i < front) {
+				scaleY = startScale + i * scaleDistance;
+		} else if (i > back) {
+				scaleY = endScale - (i - back) * scaleDistance;
+		}
+		railing.push(new Vector3(point.x, point.y + scaleY, point.z ));
+	}
+	return railing;
+}
+
+export function generateOffsetPoints(points, offsetDistance) {
+	const offsetPoints1 = [];
+	const offsetPoints2 = [];
+
+	for (let i = 0; i < points.length - 1; i++) {
+		const current = points[i];
+		const next = points[i + 1];
+
+		// Calculate the direction vector between the current and next points
+		const direction = new Vector3(next.x - current.x, next.y - current.y, next.z - current.z).normalize();
+
+		// Calculate a perpendicular vector (cross product with an arbitrary vector)
+		const perpendicular = Vector3.Cross(direction, Vector3.Up()).normalize();
+
+		// Offset the current point in both directions
+		const offset1 = new Vector3(
+			current.x + perpendicular.x * offsetDistance,
+			current.y + perpendicular.y * offsetDistance,
+			current.z + perpendicular.z * offsetDistance
+		);
+
+		const offset2 = new Vector3(
+			current.x - perpendicular.x * offsetDistance,
+			current.y - perpendicular.y * offsetDistance,
+			current.z - perpendicular.z * offsetDistance
+		);
+
+		offsetPoints1.push(offset1);
+		offsetPoints2.push(offset2);
+	}
+
+	// Add the last point offsets
+	const last = points[points.length - 1];
+	const secondLast = points[points.length - 2];
+	const lastDirection = new Vector3(last.x - secondLast.x, last.y - secondLast.y, last.z - secondLast.z).normalize();
+	const lastPerpendicular = Vector3.Cross(lastDirection, Vector3.Up()).normalize();
+
+	offsetPoints1.push(new Vector3(
+		last.x + lastPerpendicular.x * offsetDistance,
+		last.y + lastPerpendicular.y * offsetDistance,
+		last.z + lastPerpendicular.z * offsetDistance
+	));
+
+	offsetPoints2.push(new Vector3(
+		last.x - lastPerpendicular.x * offsetDistance,
+		last.y - lastPerpendicular.y * offsetDistance,
+		last.z - lastPerpendicular.z * offsetDistance
+	));
+
+	return { offsetPoints1, offsetPoints2 };
+}
+
+export function generateOffsetPointsWithBanking(points, offsetDistance, maxBankAngle) {
+	const offsetPoints1 = [];
+	const offsetPoints2 = [];
+	const totalPoints = points.length;
+
+	for (let i = 0; i < totalPoints - 1; i++) {
+		const current = points[i];
+		const next = points[i + 1];
+
+		// Calculate the direction vector between the current and next points
+		const direction = new Vector3(next.x - current.x, next.y - current.y, next.z - current.z).normalize();
+
+		// Calculate a perpendicular vector (cross product with an arbitrary vector)
+		let perpendicular = Vector3.Cross(direction, Vector3.Up()).normalize();
+
+		// Apply banking: Rotate the perpendicular vector based on the current point's position
+		const bankAngle = maxBankAngle * Math.sin((Math.PI * i) / (totalPoints - 1)); // Smooth banking curve
+		const rotationMatrix = Matrix.RotationAxis(direction, Tools.ToRadians(bankAngle));
+		perpendicular = Vector3.TransformCoordinates(perpendicular, rotationMatrix);
+
+		// Offset the current point in both directions
+		const offset1 = new Vector3(
+			current.x + perpendicular.x * offsetDistance,
+			// current.y + perpendicular.y * offsetDistance,
+			current.y,
+			current.z + perpendicular.z * offsetDistance
+		);
+
+		const offset2 = new Vector3(
+			current.x - perpendicular.x * offsetDistance,
+			// current.y - perpendicular.y * offsetDistance,
+			current.y - perpendicular.y * offsetDistance * 2,
+			current.z - perpendicular.z * offsetDistance
+		);
+
+		offsetPoints1.push(offset1);
+		offsetPoints2.push(offset2);
+	}
+
+	// Add the last point offsets
+	const last = points[totalPoints - 1];
+	const secondLast = points[totalPoints - 2];
+	const lastDirection = new Vector3(last.x - secondLast.x, last.y - secondLast.y, last.z - secondLast.z).normalize();
+	let lastPerpendicular = Vector3.Cross(lastDirection, Vector3.Up()).normalize();
+
+	const lastRotationMatrix = Matrix.RotationAxis(lastDirection, Tools.ToRadians(0)); // No bank at the end
+	lastPerpendicular = Vector3.TransformCoordinates(lastPerpendicular, lastRotationMatrix);
+
+	offsetPoints1.push(new Vector3(
+		last.x + lastPerpendicular.x * offsetDistance,
+		// last.y + lastPerpendicular.y * offsetDistance,
+		last.y,
+		last.z + lastPerpendicular.z * offsetDistance
+	));
+
+	offsetPoints2.push(new Vector3(
+		last.x - lastPerpendicular.x * offsetDistance,
+		// last.y - lastPerpendicular.y * offsetDistance,
+		last.y - lastPerpendicular.y * offsetDistance * 2,
+		last.z - lastPerpendicular.z * offsetDistance
+	));
+
+	return { offsetPoints1, offsetPoints2 };
+}
+
