45659340e85e59daa4c22fe5105278c28f80d6c3
TheVirtuoid <thevirtuoid@gmail.com>
2025-06-07 20:14:02 -0400

    racing: begin working on rendering racing

--- games/racing-game/pages/race/race.js
+++ games/racing-game/pages/race/race.js
@@ -1,8 +1,50 @@
 import {setButtons} from "../buttons.js";
+import Ui from "../../src/classes/Ui/Ui.js";
+import {HemisphericLight, Vector3} from "@babylonjs/core";
 
 export default class Race {
+	#engine;
+	#scene;
+	#canvas;
+	#camera;
+	#ui;
+	#light;
 
 	constructor() {
 		setButtons(['back', 'exit']);
+		this.#canvas = document.getElementById('world');
+		this.#ui = new Ui({
+			canvas: this.#canvas,
+			name: 'FWJS'
+		});
+		this.start();
 	}
+
+	start() {
+		this.#initialize()
+			.then(this.#render.bind(this));
+	}
+
+	async #initialize() {
+		const width = document.body.offsetWidth * .8;
+		const height = width / 16 * 9;
+		this.#canvas.width = width;
+		this.#canvas.height = height;
+		this.#engine = Ui.CreateEngine();
+		this.#scene = Ui.CreateScene();
+		this.#camera = Ui.CreateCamera({
+			position: {x: 0, y: 5, z: -10},
+			target: {x: 0, y: 0, z: 0}
+		});
+		this.#light = Ui.CreateLight({ position: { x: -1, y: 1, z: 0 } });
+		await Ui.LoadPhysics();
+	}
+
+	#render() {
+		this.#engine.runRenderLoop(() => {
+			this.#scene.render();
+		});
+	}
+
+
 }
--- games/racing-game/src/classes/Ui/Ui.js
+++ games/racing-game/src/classes/Ui/Ui.js
@@ -1,1 +1,123 @@
-export default class Ui {}
+import {
+	Color3,
+	Engine,
+	HavokPlugin,
+	HemisphericLight,
+	MeshBuilder,
+	Scene,
+	StandardMaterial,
+	UniversalCamera,
+	Vector3
+} from "@babylonjs/core";
+import HavokPhysics from "@babylonjs/havok";
+
+let instance;
+let currentName;
+
+export default class Ui {
+	library;
+	canvas;
+	scene;
+	engine;
+	name;
+	camera;
+	physicsPlugin;
+	light;
+	meshes;
+	materials;
+
+	constructor(args = {}) {
+		if (!instance) {
+			const { library, canvas, name } = args;
+			this.library = library;
+			this.canvas = canvas;
+			this.name = name;
+			this.meshes = new Map();
+			this.materials = new Map();
+			instance = this;
+		}
+		return instance;
+	}
+
+	static CreateEngine(args = {}) {
+		const { canvas = instance.canvas } = args;
+		instance.engine = new Engine(canvas, true);
+		return instance.engine;
+	}
+
+	static CreateScene(args = {}) {
+		const { engine = instance.engine } = args;
+		instance.scene = new Scene(engine);
+		return instance.scene;
+	}
+
+	static CreateCamera(args = {}) {
+		const { canvas = instance.canvas, scene = instance.scene, position, target, name = instance.name } = args;
+		const camera= new UniversalCamera(`${name}-UniversalCamera`, Ui.toVector3(position), scene);
+		camera.inputs.addMouseWheel();
+		camera.setTarget(Ui.toVector3(target));
+		camera.attachControl(canvas, true);
+		instance.camera = camera;
+		return camera;
+	}
+
+	static CreateLight(args = {}) {
+		const { name = instance.name, position, scene = instance.scene } = args;
+		instance.light = new HemisphericLight(`${name}-light`, Ui.toVector3(position), scene);
+		return instance.light;
+	}
+
+	static async LoadPhysics(args = {}) {
+		const { scene = instance.scene, gravityVector = { x: 0, y: -9.81, z: 0 } } = args;
+		instance.physicsPlugin = new HavokPlugin(true, await HavokPhysics());
+		scene.enablePhysics(gravityVector, instance.physicsPlugin);
+		return instance.physicsPlugin;
+	}
+
+	static CreateGround(args = {}) {
+		const {
+			name = 'ground',
+			scene = instance.scene,
+			width,
+			height,
+			subdivisions = 4,
+			material
+		} = args;
+		const meshName = `${instance.name}-${name}`;
+		const mesh = MeshBuilder.CreateGround(
+			meshName, { width, height, subdivisions },
+			scene);
+		if (material) {
+			mesh.material = material;
+		}
+		instance.meshes.set(meshName, mesh);
+		return mesh;
+	}
+
+	static CreateMaterial(args = {}) {
+		const { diffuseColor, name = 'material', scene = instance.scene } = args;
+		const materialName = `${instance.name}-${name}`;
+		const material = new StandardMaterial(materialName, scene);
+		if (diffuseColor) {
+			material.diffuseColor = Ui.toColor3(diffuseColor);
+		}
+		instance.materials.set(materialName, material);
+		return material;
+	}
+
+	static getMesh(name) {
+		return instance.meshes.get(name);
+	}
+
+	static getMaterial(name) {
+		return instance.materials.get(name);
+	}
+
+	static toVector3(coordinates) {
+		return new Vector3(coordinates.x, coordinates.y, coordinates.z);
+	}
+
+	static toColor3(color) {
+		return new Color3(color.r, color.g, color.b);
+	}
+}
