36c1679e577e98f357269a6336d42fc5916f2fbb
TheVirtuoid <thevirtuoid@gmail.com>
2025-06-19 11:39:10 -0400

    racing: selectVenue screen done

--- games/racing-game/pages/selectVenue/selectVenue.js
+++ games/racing-game/pages/selectVenue/selectVenue.js
@@ -1,10 +1,8 @@
 import {setButton, setButtons} from "../buttons.js";
+import venues from './../../databases/venues.json';
+import VenueDb from "../../src/classes/databases/VenueDb/VenueDb.js";
+import CarDb from "../../src/classes/databases/CarDb/CarDb.js";
 
-const venues = [
-	{id: 1, name: 'Venue 1'},
-	{id: 2, name: 'Venue 2'},
-	{id: 3, name: 'Venue 3'}
-]
 export default class SelectVenue {
 
 	#selectionList;
@@ -13,36 +11,55 @@ export default class SelectVenue {
 		this.#selectionList = document.querySelector('#venue-selection-list ul');
 
 		this.#selectionList.addEventListener('click', this.#onVenueSelect.bind(this));
+
+		VenueDb.setDatabase(JSON.stringify(venues));
+
 		this.#populateSelectionList();
 		setButtons(['back', 'exit']);
 	}
 
 	#populateSelectionList() {
 		this.#selectionList.replaceChildren();
-		venues.forEach(venue => {
-			const button = document.createElement('button');
-			button.textContent = venue.name;
-			const li = document.createElement('li');
-			li.dataset.venueId = venue.id;
-			li.appendChild(button);
-			this.#selectionList.appendChild(li);
+		const venuePromises = [];
+		const venues = VenueDb.getAllVenues();
+		venues.forEach((venue) => {
+			venuePromises.push(venue.loadVenue());
 		});
+		Promise.all(venuePromises)
+			.then(() => {
+				venues.forEach(venue => {
+					const button = document.createElement('button');
+					button.classList.add('invisible');
+					const span = document.createElement('span');
+					span.textContent = venue.name;
+					button.appendChild(venue.thumbnail);
+					button.appendChild(span);
+					const li = document.createElement('li');
+					li.dataset.venueId = venue.id;
+					li.appendChild(button);
+					this.#selectionList.appendChild(li);
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				console.log('ERROR: Could not load all venues');
+			});
+
+
 	}
 
 	#onVenueSelect(event) {
-		if (event.target.tagName === 'BUTTON') {
-			const venue = event.target.closest('li');
-			if (venue.classList.contains('selected')) {
-				venue.classList.remove('selected');
-				setButton('race', false);
-			} else {
-				const selectedVenue = this.#selectionList.querySelector('li.selected');
-				if (selectedVenue) {
-					selectedVenue.classList.remove('selected');
-				}
-				venue.classList.add('selected');
-				setButton('race', true);
+		const venue = event.target.closest('li');
+		if (venue.classList.contains('selected')) {
+			venue.classList.remove('selected');
+			setButton('race', false);
+		} else {
+			const selectedVenue = this.#selectionList.querySelector('li.selected');
+			if (selectedVenue) {
+				selectedVenue.classList.remove('selected');
 			}
+			venue.classList.add('selected');
+			setButton('race', true);
 		}
 	}
 }
