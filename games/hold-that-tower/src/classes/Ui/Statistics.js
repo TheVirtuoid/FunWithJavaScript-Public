8ea07234600b276f1fc1b7e98d7897ecdbdbe158
TheVirtuoid <thevirtuoid@gmail.com>
2025-07-13 21:56:12 -0400

    tower: The game is actually looking a little good!!

--- games/hold-that-tower/src/classes/Ui/Statistics.js
+++ games/hold-that-tower/src/classes/Ui/Statistics.js
@@ -9,6 +9,10 @@ export default class Statistics {
 	static DEFAULT_FONT = 'Tiny5';
 	static WIDTH = 360;
 	static PADDING = 10;
+	static INNER_WIDTH = Statistics.WIDTH - Statistics.PADDING * 2;
+	static LINE_GAP = 8;
+
+	#graphics;
 
 	#scene;
 	#image;
@@ -46,7 +50,6 @@ export default class Statistics {
 		this.#coins = 0;
 		this.#stars = 0;
 		this.#crowns = 0;
-		this.#gunDamage = 6;
 		this.#starImage = new Star({ scene: this.#scene});
 		this.#crownImage = new Crown({ scene: this.#scene});
 		this.#coinsImage = new Coins({ scene: this.#scene});
@@ -92,9 +95,12 @@ export default class Statistics {
 	create() {
 		const graphics = this.#scene.add.graphics();
 		this.#image = graphics;
+		let x, y;
 
-		this.#buildMainPanel();
-		this.#starImage.create();
+		({ x, y } = this.#buildMainPanel());
+		({ x, y } = this.#buildHeaderPanel(x, y));
+		({ x, y } = this.#buildStatsPanel(x, y));
+		/*this.#starImage.create();
 		this.#crownImage.create();
 		this.#coinsImage.create();
 
@@ -112,17 +118,15 @@ export default class Statistics {
 		this.#maxHealthText = this.#addMainText(new Position(250,135), 0);
 
 		this.#addMainText(new Position(30,160), 'Gun Damage');
-		this.#gunDamageText = this.#addMainText(new Position(250,160), this.#gunDamage);
+		this.#gunDamageText = this.#addMainText(new Position(250,160), 0);
 
 		this.#addMainText(new Position(30,190), 'Runner Speed');
-		this.#runnerSpeedText = this.#addMainText(new Position(250,190), this.#runnerSpeed);
+		this.#runnerSpeedText = this.#addMainText(new Position(250,190), 0);
 
 		this.#addMainText(new Position(30,220), 'Gun Rotation Speed');
-		this.#gunRotationSpeedText = this.#addMainText(new Position(250,220), 0);
+		this.#gunRotationSpeedText = this.#addMainText(new Position(250,220), 0);*/
 
-		// this.#buildEnemiesPanel();
-
-		this.#buildPrizesPanel();
+		// this.#buildPrizesPanel();
 
 		this.#scene.add.text(20, 350, 'Upgrades', {
 			fontFamily: Statistics.DEFAULT_FONT,
@@ -130,100 +134,83 @@ export default class Statistics {
 			fill: '#88ff88'
 		});
 
-		this.#scene.add.text(30, 390, 'Guns', {
-			fontFamily: Statistics.DEFAULT_FONT,
-			fontSize: '20px',
-			fill: '#ffffff'
-		});
-
-		this.#scene.add.text(100, 390, 'Tower', {
-			fontFamily: Statistics.DEFAULT_FONT,
-			fontSize: '20px',
-			fill: '#ffffff'
-		});
-
-		this.#scene.add.text(190, 390, 'Wall', {
-			fontFamily: Statistics.DEFAULT_FONT,
-			fontSize: '20px',
-			fill: '#ffffff'
-		});
-
-		this.#scene.add.text(250, 390, 'Runners', {
-			fontFamily: Statistics.DEFAULT_FONT,
-			fontSize: '20px',
-			fill: '#ffffff'
-		});
-
-		graphics.lineStyle(2, 0x8888ff);
-		graphics.lineBetween(50,430, 50, 640);
-
-		graphics.lineStyle(2, 0x8888ff);
-		graphics.lineBetween(130,430, 130, 640);
-
-		graphics.lineStyle(2, 0x8888ff);
-		graphics.lineBetween(210,430, 210, 640);
-
-		graphics.lineStyle(2, 0x8888ff);
-		graphics.lineBetween(290,430, 290, 640);
-
-		// gun
-		this.#buildUpgradePath(40, 420, [true, true, false, false, false, false]);
-
-		// tower
-		this.#buildUpgradePath(120, 420, [true, true, true, true, false, false]);
-
-		// Wall
-		this.#buildUpgradePath(200, 420, [true, true, true, true, true, false]);
-
-		// Runners
-		this.#buildUpgradePath(280, 420, [false, false, false, false, false, false]);
-
-		this.#image.fillStyle(0x000000);
-		this.#image.fillRect(0, 0, 4, 4);
-	}
-
-	#buildUpgradePath(x, y, active) {
-		for(let i = 0; i < active.length; i++) {
-			this.#buildUpgradePathBox(x, y + i * 40, active[i]);
-		}
-	}
-
-	#buildUpgradePathBox(x, y, active) {
-		const fillColor = active ? 0x228822 : 0x003300;
-		const borderColor = active ? 0x99ff99 : 0x229922;
-		this.image.fillStyle(fillColor, 1);
-		this.image.fillRoundedRect(x, y, 20, 20, 5);
-		this.image.lineStyle(2, borderColor, 1);
-		this.image.strokeRoundedRect(x, y, 20, 20, 5);
 	}
 
 	#buildMainPanel() {
-		// main panel
 		const graphics = this.#image;
 		graphics.fillStyle(Statistics.BACKGROUND_COLOR, 1);
 		graphics.fillRoundedRect(Statistics.PADDING, Statistics.PADDING, Statistics.WIDTH, this.#scene.cameras.main.height - Statistics.PADDING * 2, Statistics.PADDING * 2);
 		graphics.lineStyle(4, 0x000000,1 );
 		graphics.strokeRoundedRect(Statistics.PADDING, Statistics.PADDING, Statistics.WIDTH, this.#scene.cameras.main.height - Statistics.PADDING * 2, Statistics.PADDING * 2);
+		return { x: Statistics.PADDING * 2, y: Statistics.PADDING * 2 };
 	}
 
-	#buildPrizesPanel() {
-		this.#addPrizeText(new Position(30,300), 'Prizes:');
-		this.#coinsImage.setPosition(new Position(170, 270));
-		this.#coinsText = this.#addPrizeText(new Position(150,300), this.#coins);
-
-		this.#starImage.setPosition(new Position(240, 270));
-		this.#starsText = this.#addPrizeText(new Position(230,300), this.#stars);
-
-		this.#crownImage.setPosition(new Position(310,270), this.#coins);
-		this.#crownsText = this.#addPrizeText(new Position(300,300), this.#crowns);
+	#buildHeaderPanel(x, y) {
+		let text = this.#addExtraLargeText(new Position(x, y), 'Hold That Tower!', '#ffffff');
+		y += text.height;
+		this.#addLargeText(new Position(x, y), 'Wave:', '#88ff88');
+		this.#waveText = this.#addLargeText(new Position(x + text.width + 20, y), '0', '#88ff88');
+		return { x, y: y + this.#waveText.height };
 	}
 
-	#buildEnemiesPanel() {
-		const color = '#ff4444';
-		this.#addStandardText(new Position(30, 185), 'Enemies Destroyed: 38', color);
-		this.#addSmallText(new Position(40, 203), 'Gunners: 11', color);
-		this.#addSmallText(new Position(40, 220), 'Runners: 23', color);
-		this.#addSmallText(new Position(40, 235), 'Bosses: 4', color);
+	#buildStatsPanel(x, y) {
+		const graphics = this.#image;
+		const insetX = x + Statistics.PADDING;
+		const statsX = x + insetX + 200;
+
+		y+= Statistics.PADDING * 2;
+		const startingY = y;
+		this.#starImage.create();
+		this.#crownImage.create();
+		this.#coinsImage.create();
+
+		y += Statistics.PADDING * 2;
+		this.#addMainText(new Position(insetX, y), 'Health');
+		this.#healthText = this.#addMainText(new Position(statsX, y), 0);
+
+		y += this.#healthText.height;
+		this.#addMainText(new Position(insetX,y), 'Max Health');
+		this.#maxHealthText = this.#addMainText(new Position(statsX,y), 0);
+
+		y += this.#maxHealthText.height;
+		this.#addMainText(new Position(insetX,y), 'Gun Damage');
+		this.#gunDamageText = this.#addMainText(new Position(statsX,y), 0);
+
+		y += this.#gunDamageText.height;
+		this.#addMainText(new Position(insetX,y), 'Runner Speed');
+		this.#runnerSpeedText = this.#addMainText(new Position(statsX,y), 0);
+
+		y += this.#runnerSpeedText.height;
+		this.#addMainText(new Position(insetX,y), 'Gun Rotation Speed');
+		this.#gunRotationSpeedText = this.#addMainText(new Position(statsX,y), 0);
+
+		y += this.#gunRotationSpeedText.height;
+
+		({ x, y } = this.#buildPrizesPanel(x, y));
+
+		graphics.lineStyle(2, 0xffffff,1);
+
+		y += Statistics.PADDING;
+		graphics.strokeRoundedRect(x, startingY, Statistics.INNER_WIDTH, y - startingY, 20);
+
+		return { x, y }
+	}
+
+	#buildPrizesPanel(x, y) {
+		y += Statistics.PADDING * 4;
+		const insetX = x + Statistics.PADDING;
+		this.#coinsImage.setPosition(new Position(insetX + 150, y));
+		this.#starImage.setPosition(new Position(insetX + 220, y));
+		this.#crownImage.setPosition(new Position(insetX + 290, y));
+
+		y += (this.#coinsImage.image.height * this.#coinsImage.image.scale) - Statistics.PADDING * 2 + 5;
+		this.#addPrizeText(new Position(insetX, y), 'Prizes:');
+		this.#coinsText = this.#addPrizeText(new Position(insetX + 130,y), 0);
+		this.#starsText = this.#addPrizeText(new Position(insetX + 200,y), 0);
+		this.#crownsText = this.#addPrizeText(new Position(insetX + 270,y), 0);
+
+		y += this.#coinsText.height;
+		return { x, y }
 	}
 
 	#addMainText(position, text) {
